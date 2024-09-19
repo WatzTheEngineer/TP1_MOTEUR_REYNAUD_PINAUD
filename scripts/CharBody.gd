8ed784ea9bdae6acb85a1df014a4b6c05618ba79
Watz <watztheengineer@gmail.com>
2024-09-19 17:07:40 -0400

    Add player animations

--- scripts/CharBody.gd
+++ scripts/CharBody.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 80
+@onready var _animation_player = $AnimatedSprite2D
 
 func get_input():
 	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
@@ -9,3 +10,15 @@ func get_input():
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
+
+func _process(_delta):
+	if Input.is_action_pressed("Down"):
+		_animation_player.play("walk_down")
+	elif Input.is_action_pressed("Up"):
+		_animation_player.play("walk_up")
+	elif Input.is_action_pressed("Right"):
+		_animation_player.play("walk_right")
+	elif Input.is_action_pressed("Left"):
+		_animation_player.play("walk_left")
+	else:
+		_animation_player.play("idle")
